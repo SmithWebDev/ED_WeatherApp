dabf53eedc867f4378b231c2b7b24dbfb7714670
Smith <smithwebdd@gmail.com>
2019-08-19 11:15:28 -0400

    set up test view and controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,12 @@
 class WelcomeController < ApplicationController
   def test
+    response = HTTParty.get("http://api.openweathermap.org/data/2.5/weather?zip=28204&units=imperial&appid=#{ENV['openweather_api_key']}")
+    @location = response['name']
+    @temp = response['main']['temp']
+    @weather_icon = response['weather'][0]['icon']
+    @weather_words = response['weather'][0]['description']
+    @cloudiness = response['clouds']['all']
+    @windiness = response['wind']['speed']
   end
 
   def index
